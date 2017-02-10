2e2404ffa406fa4ee13df7028a966f54f2b6ffef
danaguinaldo <dan.r.aguinaldo@gmail.com>
2017-02-09 22:45:10 -0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 var screen_xs = 767;
 
+if(window.innerWidth <= screen_xs) {
+    $(".navbar").addClass("navbar-fixed-top");
+    $(".section-title").click(function() {
+        $(this).parent().find('.content').toggleClass("content-open");
+        if ($(this).parent().find('.content').hasClass("content-open")) {
+            $(this).find('i').removeClass('fa-caret-down');
+            $(this).find('i').addClass('fa-caret-up');
+        } else {
+            $(this).find('i').removeClass('fa-caret-up');
+            $(this).find('i').addClass('fa-caret-down');
+        }
+    });
+} else {
+    $(".navbar").removeClass("navbar-fixed-top");
+    $(".content").each(function() {
+        $(this).removeClass("content-open");
+    });
+}
+
 $(document).ready(function() {
     $('.burger').click(function() {
         $('ul').toggleClass('open');
@@ -8,25 +27,6 @@ $(document).ready(function() {
     $('ul li a').click(function() {
         $('ul').removeClass('open');
     });
-
-    if(window.innerWidth <= screen_xs) {
-        $(".navbar").addClass("navbar-fixed-top");
-        $(".section-title").click(function() {
-            $(this).parent().find('.content').toggleClass("content-open");
-            if ($(this).parent().find('.content').hasClass("content-open")) {
-                $(this).find('i').removeClass('fa-caret-down');
-                $(this).find('i').addClass('fa-caret-up');
-            } else {
-                $(this).find('i').removeClass('fa-caret-up');
-                $(this).find('i').addClass('fa-caret-down');
-            }
-        });
-    } else {
-        $(".navbar").removeClass("navbar-fixed-top");
-        $(".content").each(function() {
-            $(this).removeClass("content-open");
-        });
-    }
 
     $(window).resize(function() {
         if (window.innerWidth <= screen_xs) {
